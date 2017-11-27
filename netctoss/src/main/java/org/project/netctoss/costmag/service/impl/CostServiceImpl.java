33538460b5ffaf6e19df8bfbeaafaee451215ab4
Yance <Yance@DESKTOP-HNOD81R>
2017-11-27 16:31:54 +0800

    实现CoseService接口

---
+++ netctoss/src/main/java/org/project/netctoss/costmag/service/impl/CostServiceImpl.java
@@ -0,0 +1,43 @@
+package org.project.netctoss.costmag.service.impl;
+
+import javax.annotation.Resource;
+
+import org.project.netctoss.beans.CostBean;
+import org.project.netctoss.costmag.dao.ICostDao;
+import org.project.netctoss.costmag.service.ICostService;
+import org.project.netctoss.pojos.PagerBean;
+
+public class CostServiceImpl implements ICostService {
+	@Resource
+	private ICostDao costDaoImpl;
+	@Override
+	public void insertCosts(CostBean costBean) {
+		// TODO Auto-generated method stub
+		costDaoImpl.insertCosts(costBean);
+	}
+
+	@Override
+	public CostBean selectCosts(long id) {
+		// TODO Auto-generated method stub
+		return costDaoImpl.selectCosts(id);
+	}
+
+	@Override
+	public void deleteCosts(CostBean costBean) {
+		// TODO Auto-generated method stub
+		costDaoImpl.deleteCosts(costBean);
+	}
+
+	@Override
+	public void updateCosts(CostBean costBean) {
+		// TODO Auto-generated method stub
+		costDaoImpl.updateCosts(costBean);
+	}
+
+	@Override
+	public PagerBean findAllCostsByPager(PagerBean pager) {
+		// TODO Auto-generated method stub
+		return costDaoImpl.findAllCostsByPager(pager);
+	}
+
+}

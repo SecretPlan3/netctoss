bbacc0a82f2d91ec6863ae453a529fd68712cb2e
Yance <Yance@DESKTOP-HNOD81R>
2017-11-28 17:10:28 +0800

    1.0

--- netctoss/src/test/java/testCostService/TestCostService.java
+++ netctoss/src/test/java/testCostService/TestCostService.java
@@ -1,5 +1,24 @@
 package testCostService;
 
-public class TestCostService {
+import javax.annotation.Resource;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.project.netctoss.beans.CostBean;
+import org.project.netctoss.costmag.service.ICostService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class TestCostService {
+	@Resource
+	private ICostService costServiceImpl;
+	
+	@Test
+	public void TestSelectCosts() {
+		CostBean cb = costServiceImpl.selectCosts(1);
+		System.out.println(cb);
+	}
+	
 }

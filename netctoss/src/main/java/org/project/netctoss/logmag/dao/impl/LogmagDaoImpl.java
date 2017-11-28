6e874b2ac61e63e038303e8f2a9523311e534f6b
Yance <Yance@DESKTOP-HNOD81R>
2017-11-28 16:46:13 +0800

    更新Log

--- netctoss/src/main/java/org/project/netctoss/logmag/dao/impl/LogmagDaoImpl.java
+++ netctoss/src/main/java/org/project/netctoss/logmag/dao/impl/LogmagDaoImpl.java
@@ -2,22 +2,77 @@ package org.project.netctoss.logmag.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.project.netctoss.beans.ManagerLogBean;
 import org.project.netctoss.logmag.dao.ILogmagDao;
 import org.project.netctoss.pojos.PagerBean;
-
-public class LogmagDaoImpl implements ILogmagDao {
+import org.project.netctoss.utils.BaseDao;
+import org.springframework.stereotype.Repository;
+@Repository
+public class LogmagDaoImpl extends BaseDao implements ILogmagDao {
 
 	@Override
-	public PagerBean findManagerLog(PagerBean pagerBean) {
+	public PagerBean findManagerLog(PagerBean pager) {
 		// TODO Auto-generated method stub
-		return null;
+		//根据管理查询登陆/退出系统时间
+		StringBuilder hql = new StringBuilder("select count(l.id)from ManagerLogBean as l where l.fk_manager_id = :managerId");
+		Query query = getSession().createQuery(hql.toString());
+		query.setProperties(pager.getParams());
+		int totalRows = (int) query.uniqueResult();
+		pager.setTotalRows(Integer.valueOf(String.valueOf(totalRows)));
+		
+		hql = new StringBuilder("from ManagerLogBean as l where l.fk_manager_id = :managerId");
+		query = getSession().createQuery(hql.toString());
+		query.setProperties(pager.getParams());
+		query.setFirstResult(pager.getIndex());
+		query.setMaxResults(pager.getRows());
+		List<?> datas = query.list();
+		pager.setDatas(datas);
+		return pager;
 	}
 
 	@Override
-	public PagerBean findManagerOperationLog(PagerBean pagerBean) {
+	public PagerBean findManagerOperationLog(PagerBean pager) {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder hql = new StringBuilder("select count(l.id)from ManagerOperationLogBean as l where 1 = 1");
+		if(pager.getParams().get("managerId") != null) {
+			//根据管理员查询该管理操作信息
+			hql.append(" and l.fk_manager_id = :managerId");
+		}
+		if(pager.getParams().get("LoginTime") != null) {
+			//根据登陆日期范围查询该管理员操作信息
+			hql.append(" and l.operation_time > :LoginTime");
+		}
+		if(pager.getParams().get("LogoutTime") != null) {
+			//根据退出日期范围查询该管理员操作信息
+			hql.append(" and l.operation_time < :LogoutTime");
+		}
+		Query query = getSession().createQuery(hql.toString());
+		query.setProperties(pager.getParams());
+		int totalRows = (int) query.uniqueResult();
+		pager.setTotalRows(Integer.valueOf(String.valueOf(totalRows)));
+		
+		hql = new StringBuilder("from ManagerOperationLogBean as l where 1 = 1");
+		if(pager.getParams().get("managerId") != null) {
+			//根据管理员查询该管理操作信息
+			hql.append(" and l.fk_manager_id = :managerId");
+		}
+		if(pager.getParams().get("LoginTime") != null) {
+			//根据登陆日期范围查询该管理员操作信息
+			hql.append(" and l.operation_time > :LoginTime");
+		}
+		if(pager.getParams().get("LogoutTime") != null) {
+			//根据退出日期范围查询该管理员操作信息
+			hql.append(" and l.operation_time < :LogoutTime");
+		}
+		
+		query = getSession().createQuery(hql.toString());
+		query.setProperties(pager.getParams());
+		query.setFirstResult(pager.getIndex());
+		query.setMaxResults(pager.getRows());
+		List<?> datas = query.list();
+		pager.setDatas(datas);
+		return pager;
 	}
 
 }

457b41a575e21cb597181414884bc19653b19aca
lenovo <lenovo@lenovo-PC.mshome.net>
2017-11-28 18:20:12 +0800

    常规提交 Service

--- netctoss/src/main/java/org/project/netctoss/beans/ServiceBean.java
+++ netctoss/src/main/java/org/project/netctoss/beans/ServiceBean.java
@@ -16,7 +16,119 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
-
+@Entity
+@Table(name="t_service")
 public class ServiceBean {
-
+	@Id
+	@GenericGenerator(name="hibernate.identity",strategy="identity")
+	@GeneratedValue(generator="hibernate.identity")
+	  private String id;
+	
+	@Column(name="unix_host",length=15)
+	  private String unixHost;
+	
+	@Column(name="os_username",length=20)
+	  private String osName;
+	@Column(name="password",length=32)
+	  private String osPassword;
+	@Column(name="status")
+	  private Integer status;
+	@Column(name="create_date")
+	  private Date createDate;
+	@Column(name="pause_date")
+	  private Date pauseDate;
+	@Column(name="close_date")
+	  private Date closeDate;
+	@ManyToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+	@JoinColumn(name="fk_user_id")
+	  private UserBean user;
+	@ManyToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+	@JoinColumn(name="fk_cost_id")
+	private CostBean cost;
+	
+	@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.LAZY,mappedBy="service")
+	private Set<ServiceDetailCostBean>  serviceDetailCostSet;
+	
+	@OneToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY,mappedBy="service")
+	private ServiceYearlyBean serviceYear;
+	
+	public String getUnixHost() {
+		return unixHost;
+	}
+	public void setUnixHost(String unixHost) {
+		this.unixHost = unixHost;
+	}
+	public String getOsName() {
+		return osName;
+	}
+	public void setOsName(String osName) {
+		this.osName = osName;
+	}
+	public String getOsPassword() {
+		return osPassword;
+	}
+	public void setOsPassword(String osPassword) {
+		this.osPassword = osPassword;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Date getPauseDate() {
+		return pauseDate;
+	}
+	public void setPauseDate(Date pauseDate) {
+		this.pauseDate = pauseDate;
+	}
+	public Date getCloseDate() {
+		return closeDate;
+	}
+	public void setCloseDate(Date closeDate) {
+		this.closeDate = closeDate;
+	}
+	public UserBean getUser() {
+		return user;
+	}
+	public void setUser(UserBean user) {
+		this.user = user;
+	}
+	public CostBean getCost() {
+		return cost;
+	}
+	public void setCost(CostBean cost) {
+		this.cost = cost;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Set<ServiceDetailCostBean> getServiceDetailCostSet() {
+		return serviceDetailCostSet;
+	}
+	public void setServiceDetailCostSet(Set<ServiceDetailCostBean> serviceDetailCostSet) {
+		this.serviceDetailCostSet = serviceDetailCostSet;
+	}
+	public ServiceYearlyBean getServiceYear() {
+		return serviceYear;
+	}
+	public void setServiceYear(ServiceYearlyBean serviceYear) {
+		this.serviceYear = serviceYear;
+	}
+	@Override
+	public String toString() {
+		return "ServiceBean [unixHost=" + unixHost + ", osName=" + osName + ", osPassword=" + osPassword + ", status="
+				+ status + ", createDate=" + createDate + ", pauseDate=" + pauseDate + ", closeDate=" + closeDate + "]";
+	}
+	  
 }

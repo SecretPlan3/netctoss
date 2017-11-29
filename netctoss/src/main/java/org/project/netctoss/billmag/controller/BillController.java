c5fd8ac74138c5c419fe49e3d1561d6d454fa4e0
Administrator <Administrator@192.168.23.7>
2017-11-29 15:10:14 +0800

    controller

---
+++ netctoss/src/main/java/org/project/netctoss/billmag/controller/BillController.java
@@ -0,0 +1,26 @@
+package org.project.netctoss.billmag.controller;
+
+import javax.annotation.Resource;
+
+import org.project.netctoss.billmag.service.IBillService;
+import org.project.netctoss.pojos.PagerBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/bill")
+public class BillController {
+	
+	@Resource
+	private IBillService billServiceImpl;
+	
+	@RequestMapping(value="/findUserBill",method = { RequestMethod.GET },produces= {"application/json;charset=utf-8"})
+	public PagerBean findUserBill(PagerBean pb) {
+		System.out.println(pb);
+		pb = billServiceImpl.findAllUserBillByCondition(pb);
+		return pb;
+	}
+	
+}

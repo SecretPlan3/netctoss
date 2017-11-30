f00c2c3b6fb087469cd94b3dfe2e0e288cb14792
Yance <Yance@192.168.23.2>
2017-11-30 09:44:41 +0800

    增加测试方法

--- netctoss/src/test/java/testCostService/TestCostService.java
+++ netctoss/src/test/java/testCostService/TestCostService.java
@@ -1,5 +1,8 @@
 package testCostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.junit.Ignore;
@@ -7,15 +10,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.project.netctoss.beans.CostBean;
 import org.project.netctoss.costmag.service.ICostService;
+import org.project.netctoss.pojos.PagerBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sun.jmx.snmp.daemon.CommunicatorServerMBean;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
 public class TestCostService {
 	@Resource
 	private ICostService costServiceImpl;
-	
+	@Test
+	public void testFindAllCostsBypage() {
+		PagerBean pb = new PagerBean();
+		pb.setIndex(1);
+		pb.setRows(3);
+		Map<String,Object> params = new HashMap<>();
+		params.put("type", 1);
+		pb.setParams(params);
+		
+		pb = costServiceImpl.findAllCostsByPager(pb);
+	}
 	@Ignore
 	public void testUpdateCosts() {
 		CostBean cb = new CostBean();
@@ -24,11 +40,9 @@ public class TestCostService {
 		cb.setDescription("哎哟不错哦");
 		costServiceImpl.updateCosts(cb);
 	}
-	@Test
+	@Ignore
 	public void testDelectCosts() {
-		//程序执行成功但是数据库没有反应
-		CostBean cb = costServiceImpl.selectCosts(1);
-		cb = new CostBean();
+		CostBean cb = costServiceImpl.selectCosts(4);
 		System.out.println(cb);
 		costServiceImpl.deleteCosts(cb);
 	}
@@ -43,7 +57,7 @@ public class TestCostService {
 	@Ignore
 	public void TestSelectCosts() {
 		CostBean cb = costServiceImpl.selectCosts(1);
+		costServiceImpl.deleteCosts(cb);
 		System.out.println(cb);
-	}
-	
+	}	
 }

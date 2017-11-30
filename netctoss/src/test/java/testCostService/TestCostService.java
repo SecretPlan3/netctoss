ddec169a5b97765ab028c2a9b59ae348c4197b14
Yance <Yance@192.168.23.2>
2017-11-30 09:19:04 +0800

    增加TestCostService方法

--- netctoss/src/test/java/testCostService/TestCostService.java
+++ netctoss/src/test/java/testCostService/TestCostService.java
@@ -2,6 +2,7 @@ package testCostService;
 
 import javax.annotation.Resource;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.project.netctoss.beans.CostBean;
@@ -15,7 +16,31 @@ public class TestCostService {
 	@Resource
 	private ICostService costServiceImpl;
 	
+	@Ignore
+	public void testUpdateCosts() {
+		CostBean cb = new CostBean();
+		cb.setId(2);
+		cb.setName("超级卡");
+		cb.setDescription("哎哟不错哦");
+		costServiceImpl.updateCosts(cb);
+	}
 	@Test
+	public void testDelectCosts() {
+		//程序执行成功但是数据库没有反应
+		CostBean cb = costServiceImpl.selectCosts(1);
+		cb = new CostBean();
+		System.out.println(cb);
+		costServiceImpl.deleteCosts(cb);
+	}
+	@Ignore
+	public void testInsertCosts() {
+		CostBean cb = new CostBean();
+		cb.setName("冰淇淋套餐");
+		cb.setCostNumber("xx120");
+		costServiceImpl.insertCosts(cb);
+		
+	}
+	@Ignore
 	public void TestSelectCosts() {
 		CostBean cb = costServiceImpl.selectCosts(1);
 		System.out.println(cb);
